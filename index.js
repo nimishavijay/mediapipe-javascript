12106e79e403b45cbf33ca681f56bd7a2d56b517
nim <nimisha.k.vijay@gmail.com>
2021-05-05 18:29:25 +0530

    console log results

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
 
 function onResults(results) {
-  canvasCtx.save();
-  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(
-      results.image, 0, 0, canvasElement.width, canvasElement.height);
-  drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
-                 {color: '#00FF00', lineWidth: 4});
-  drawLandmarks(canvasCtx, results.poseLandmarks,
-                {color: '#FF0000', lineWidth: 2});
-  canvasCtx.restore();
+  // canvasCtx.save();
+  // canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  // canvasCtx.drawImage(
+  //     results.image, 0, 0, canvasElement.width, canvasElement.height);
+  // drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
+  //                {color: '#00FF00', lineWidth: 4});
+  // drawLandmarks(canvasCtx, results.poseLandmarks,
+  //               {color: '#FF0000', lineWidth: 2});
+  // canvasCtx.restore();
+
+	console.log(results)
 }
 
 const pose = new Pose({locateFile: (file) => {
